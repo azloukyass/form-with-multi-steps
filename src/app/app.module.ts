4ab7e43e5286fa1357b6881260e95939d649ef33
azloukyassine <mohamed-yassine.azlouk@adesso.de>
2023-04-06 12:21:46 +0200

    added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { MyFormComponent } from './components/my-form/my-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormlyModule} from '@ngx-formly/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { MatStepperModule } from '@angular/material/stepper';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/step-type.ts
@@ -0,0 +1,5 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+export interface StepType {
+  label: string;
+  fields: FormlyFieldConfig[];
+}
